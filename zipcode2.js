5eada24eb8cb5520df2140bf3aeda5d261193986
kelvin obiri <obirikan020@gmail.com>
2023-03-31 15:02:49 +0000

    dddd

---
+++ zipcode2.js
@@ -0,0 +1,12 @@
+    // const url = 'https://api.zippopotam.us/us/';
+    // // Define the zip code you want to look up
+    // const zipCode = zipcode;
+    // // Make the API request using Axios
+    // await axios.get(`${url}${zipCode}`).then((response) => {
+    //     // Access the city name from the API response
+    //     const cityName =response.data.places[0]["place name"];
+    //     console.log(`The city name for zip code ${zipCode} is ${cityName}.`);
+    //     register(email, password, username, zipcode, cityName, address);
+    //   }).catch((error) => {
+    //     console.log(error);
+    //   });

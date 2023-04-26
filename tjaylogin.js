b47af0fae51f88c40ceea672e715be03540ce491
kelvin obiri <obirikan020@gmail.com>
2023-04-26 22:07:53 +0000

    Update tjaylogin.js

--- tjaylogin.js
+++ tjaylogin.js
@@ -21,6 +21,47 @@
           }
   });
   }
+  
+//firebase.js
+import { initializeApp } from "firebase/app";
+import {getAuth,GoogleAuthProvider} from "firebase/auth"
+import {getFirestore} from '@firebase/firestore'
+import { getStorage } from "firebase/storage";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfWHMI_JjXXjDlgcsGCN9J5TN1klpkXGA",
+  authDomain: "payment-form-34f25.firebaseapp.com",
+  projectId: "payment-form-34f25",
+  storageBucket: "payment-form-34f25.appspot.com",
+  messagingSenderId: "1040352949790",
+  appId: "1:1040352949790:web:e67579251a1f9e38a8a00f"
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth =getAuth(app)
+export const storage=getStorage(app)
+export const provider=new GoogleAuthProvider()
+export const db =getFirestore(app)
+export default app
+/////////////end of firebasefile///////////  
+  
+         //send data of products to the database
+        const sub=async()=> {
+           let sc=collection(db,'students')
+            try {
+                const result=await addDoc(sc,{
+                 
+                }))
+                console.log(result);
+            } catch (error) {
+                console.log(error.message)
+            }
+            
+          }
+
 
  async function LogIn(){
    await signInWithEmailAndPassword(auth,email.value,password.value)

4680f1851c91bac920c5486e8d658e296dfbdfee
kelvin obiri <obirikan020@gmail.com>
2023-07-14 13:40:35 -0700

    Create works.js

---
+++ works.js
@@ -0,0 +1,21 @@
+
+const fetchProducts = async () => {
+      const db = getFirestore();
+      const productsCollection = collection(db, "Products");
+
+      const productsQuery = query(
+        productsCollection,
+        where("vid", "==", current.aid)
+      );
+
+      const snapshot = await getDocs(productsQuery);
+      const fetchedProducts = [];
+      snapshot.forEach((doc) => {
+        const product = { id: doc.id, ...doc.data() };
+        fetchedProducts.push(product);
+      });
+
+      setLoading(false);
+      setAllProducts(fetchedProducts);
+      setFilteredProducts(fetchedProducts);
+    };

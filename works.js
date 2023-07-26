19e26d20ff32af67b55ab079f6ed69943ecdc657
kelvin obiri <obirikan020@gmail.com>
2023-07-26 07:42:46 -0700

    Update works.js

--- works.js
+++ works.js
@@ -19,3 +19,32 @@ const fetchProducts = async () => {
       setAllProducts(fetchedProducts);
       setFilteredProducts(fetchedProducts);
     };
+
+
+//adds a timestamp to all collection in the Products collection
+async function addTimestampToProducts() {
+  try {
+    const productsRef = collection(db, 'Products');
+
+    // Create a query for all products without a timestamp
+    const q = query(productsRef);
+
+    // Get the product IDs that match the query
+    const querySnapshot = await getDocs(q);
+    const productIds = querySnapshot.docs.map((doc) => doc.id);
+    console.log({ productIds });
+
+    // Update each product with the current timestamp
+    const currentTimestamp = serverTimestamp();
+    
+    for (const productId of productIds) {
+      const productRef = doc(productsRef, productId);
+      await updateDoc(productRef, { timestamp: currentTimestamp }); // Update or set the 'timestamp' field with the current timestamp
+    }
+
+    console.log('Timestamps added to all products.');
+  } catch (error) {
+    console.error('Error adding timestamps:', error);
+  }
+}
+
